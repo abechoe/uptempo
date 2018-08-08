2f8cd78cbfe0a57845e3ea964059bc7795f9b046
Abraham Choe <abraham.choe@gmail.com>
2018-08-08 11:48:23 -0700

    Add webpack config and boilerplate react code

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+
+const DIST_DIR = path.resolve('dist');
+const CLIENT_DIR = path.resolve('src');
+
+module.exports = {
+  context: path.join(CLIENT_DIR, 'entries'),
+  entry: {
+    main: './main.jsx',
+  },
+  output: {
+    path: DIST_DIR,
+    filename: '[name].js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: [
+          'babel-loader',
+        ],
+      },
+    ],
+  },
+  resolve: {
+    modules: [
+      CLIENT_DIR,
+      path.resolve('node_modules'),
+    ],
+    extensions: [
+      '.js',
+      '.jsx',
+    ],
+  },
+}
