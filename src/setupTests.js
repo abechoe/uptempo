6cdc9e0ca98492daf0e312aa79b34eadf13faf32
Abraham Choe <abraham.choe@gmail.com>
2018-08-31 21:43:46 -0700

    Update configuration to make tests runnable

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,4 +1,6 @@
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
-configure({adapter: new Adapter() });
+configure({ adapter: new Adapter() });
+
+global.navigator = { userAgent: 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36' };
