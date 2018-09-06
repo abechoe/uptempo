81b5807daad07c3518a99f1aef8ca7d4cbc8e2b7
Abraham Choe <abraham.choe@gmail.com>
2018-09-06 12:04:41 -0700

    Resolve absolute component paths in jest config

--- jest.config.js
+++ jest.config.js
@@ -73,6 +73,7 @@ module.exports = {
   // A map from regular expressions to module names that allow to stub out resources with a single module
   moduleNameMapper: {
     "^modules/(.*)": "<rootDir>/src/modules/$1",
+    "^components/(.*)": "<rootDir>/src/components/$1",
   },
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
