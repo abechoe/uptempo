6af69e0f9dc1cfb1a806b0380cee6b044d960a7a
Abraham Choe <abraham.choe@gmail.com>
2018-09-05 17:33:15 -0700

    Add EventForm tests and snapshots

---
+++ src/__tests__/EventForm.test.jsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import { shallow, mount } from 'enzyme';
+
+import EventForm from '../components/EventForm';
+
+describe('EventForm component tests', function() {
+  it('renders an EventForm component', function() {
+    const wrapper = shallow(<EventForm />);
+    expect(wrapper).toMatchSnapshot();
+  });
+});
+
+describe('EventForm submit tests', function() {
+  let mockEvent;
+  let mockInputValues;
+  let mockCreatedEvent;
+
+  beforeEach(() => {
+    mockEvent = { preventDefault: jest.fn() };
+    mockInputValues = {
+      name: 'Sunday Service',
+      event_start: '2018-09-06T02:00',
+      event_end: '2018-09-06T03:00',
+      recurs: true,
+      _radiumStyleState: {},
+    };
+
+    mockCreatedEvent = {
+      "name":"Sunday Service",
+      "event_start":"2018-09-06 02:00:00 UTC",
+      "event_end":"2018-09-06 03:00:00 UTC",
+      "recurs":true,
+      "interval":null,
+      "tasks":[]
+    };
+
+    global.fetch = jest.fn().mockImplementation(() => Promise.resolve({
+      json: () => Promise.resolve({
+        event: mockCreatedEvent,
+      })
+    }));
+  });
+
+  it('calls fetch with the supplied input values', function() {
+    const wrapper = shallow(<EventForm />);
+    const expectedFetchBody = {
+      method: 'POST',
+      body: JSON.stringify(mockInputValues),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }
+
+    wrapper.setState(mockInputValues);
+    wrapper.instance().handleSubmit(mockEvent);
+    expect(global.fetch)
+      .toHaveBeenCalledWith(`${process.env.REACT_APP_API_URL}/events`, expectedFetchBody);
+  });
+});
